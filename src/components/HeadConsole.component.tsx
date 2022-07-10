08988f07ea1943df353e2f681c97c1e08071e2c0
M-WRI <me@mwri.de>
2022-07-11 00:09:11 +0200

    name on head console from redux storage

--- src/components/HeadConsole.component.tsx
+++ src/components/HeadConsole.component.tsx
@@ -1,15 +1,16 @@
-// COMPONENTS
-import { Button } from "./Button.component";
-import { Icon } from "./Icon.component";
 import type { RootState } from "../store/store";
 import { useSelector } from "react-redux";
+// COMPONENTS
+import { Headline } from "./Headline.component";
+import { Button } from "./Button.component";
+import { Icon } from "./Icon.component";
 // STYLES
 import styles from "../styles/HeadConsole.module.scss";
 
 export const HeadConsole = () => {
   const user = useSelector((state: RootState) => state.user);
 
-  console.log(user.firstName);
+  console.log(user, "<--");
 
   return (
     <header className={styles.headConsoleContainer}>
@@ -19,7 +20,18 @@ export const HeadConsole = () => {
           <div className={styles.border} />
           <Button type="L">Raumstation Berlin</Button>
         </div>
-        <div></div>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileImageContainer}>
+            {user.profileImage ? (
+              <img src={user.profileImage} alt={user.firstName} />
+            ) : (
+              <Headline type="h2">
+                {`${user.firstName.charAt(0)}${user.lastName.charAt(0)}`}
+              </Headline>
+            )}
+          </div>
+          <Button type="L">{`${user.firstName} ${user.lastName}`}</Button>
+        </div>
       </div>
     </header>
   );
