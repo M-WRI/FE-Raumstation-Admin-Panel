707c43550a03700179fde15eb60fbf40324769f5
M-WRI <me@mwri.de>
2022-07-10 23:47:19 +0200

    created with redux a user state

--- src/components/HeadConsole.component.tsx
+++ src/components/HeadConsole.component.tsx
@@ -1,10 +1,16 @@
 // COMPONENTS
 import { Button } from "./Button.component";
 import { Icon } from "./Icon.component";
+import type { RootState } from "../store/store";
+import { useSelector } from "react-redux";
 // STYLES
 import styles from "../styles/HeadConsole.module.scss";
 
 export const HeadConsole = () => {
+  const user = useSelector((state: RootState) => state.user);
+
+  console.log(user.firstName);
+
   return (
     <header className={styles.headConsoleContainer}>
       <div className={styles.wrapper}>
