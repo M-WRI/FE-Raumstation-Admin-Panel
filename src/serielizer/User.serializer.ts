3b956927aeac3b294b9197f62614121e244f0947
M-WRI <me@mwri.de>
2022-07-10 23:19:48 +0200

    created user mock & serializer

---
+++ src/serielizer/User.serializer.ts
@@ -0,0 +1,21 @@
+import { IUser, IUserResponse } from "../types/user.types";
+
+export const UserSerializer = (res: IUserResponse): IUser => {
+  return {
+    id: res.id,
+    role: res.role,
+    profileImage: res.profile_image,
+    firstName: res.first_name,
+    lastName: res.last_name,
+    email: res.email,
+    floor: res.floor,
+    seat: res.seat,
+
+    companyDetails: {
+      id: res.company_details.id,
+      companyName: res.company_details.company_name,
+      description: res.company_details.description,
+      industry: res.company_details.industry,
+    },
+  };
+};
